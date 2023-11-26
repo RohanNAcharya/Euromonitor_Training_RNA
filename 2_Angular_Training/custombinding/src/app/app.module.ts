2df6cd2c21d8a8237db67fcc8563a973961dd149
RohanNAcharya <rohannacharya@gmail.com>
2023-11-26 13:04:41 +0530

    E Learning app

---
+++ 2_Angular_Training/custombinding/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CoursesComponent } from './courses/courses.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 2_Angular_Training/custombinding/src/app/courses/courses.component.ts
@@ -0,0 +1,122 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-courses',
+  templateUrl: './courses.component.html',
+  styleUrl: './courses.component.css'
+})
+export class CoursesComponent {
+
+  courses = [
+    {
+      id: 101,
+      name: "Angular for Beginners",
+      author: "Steve Smith",
+      duration: 19.5,
+      type: "Free",
+      price: 0.00,
+      ratings: 4.8,
+      image: 'assets/courses/course-img-1.png',
+      description: 'In this course, you will learn the fundamentals of Angular. The course is purely project-based.'
+    },
+    {
+      id: 102,
+      name: "React Fundamentals",
+      author: "Emily Johnson",
+      duration: 15.0,
+      type: "Paid",
+      price: 29.99,
+      ratings: 4.5,
+      image: 'assets/courses/course-img-2.png',
+      description: 'This React fundamentals course covers the core concepts and best practices in building React applications.'
+    },
+    {
+      id: 103,
+      name: "Java Programming Mastery",
+      author: "John Davis",
+      duration: 25.0,
+      type: "Paid",
+      price: 49.99,
+      ratings: 4.7,
+      image: 'assets/courses/course-img-3.webp',
+      description: 'Master the art of Java programming with this comprehensive course covering advanced topics and real-world projects.'
+    },
+    {
+      id: 104,
+      name: "Django Web Development",
+      author: "Sophia Miller",
+      duration: 22.5,
+      type: "Paid",
+      price: 39.99,
+      ratings: 4.6,
+      image: 'assets/courses/course-img-4.jpg',
+      description: 'Build powerful web applications with Django. This course includes hands-on projects and best practices in Django development.'
+    },
+    {
+      id: 105,
+      name: "Flask for Beginners",
+      author: "Alex Turner",
+      duration: 18.0,
+      type: "Free",
+      price: 0.00,
+      ratings: 4.5,
+      image: 'assets/courses/course-img-5.png',
+      description: 'Learn the basics of Flask and create your first web application. This course is perfect for beginners in web development.'
+    },
+    {
+      id: 106,
+      name: "ExpressJS Masterclass",
+      author: "Daniel White",
+      duration: 20.0,
+      type: "Paid",
+      price: 34.99,
+      ratings: 4.8,
+      image: 'assets/courses/course-img-6.jpg',
+      description: 'Become an ExpressJS expert with this comprehensive masterclass. Includes real-world examples and advanced topics.'
+    },
+    {
+      id: 107,
+      name: "TypeScript Essentials",
+      author: "Lily Thompson",
+      duration: 14.5,
+      type: "Free",
+      price: 0.00,
+      ratings: 4.7,
+      image: 'assets/courses/course-img-7.webp',
+      description: 'Get started with TypeScript and enhance your JavaScript skills. This course covers the basics and advanced features of TypeScript.'
+    },
+    {
+      id: 108,
+      name: "Python for Data Science",
+      author: "Michael Robinson",
+      duration: 30.0,
+      type: "Paid",
+      price: 59.99,
+      ratings: 4.9,
+      image: 'assets/courses/course-img-8.png',
+      description: 'Unlock the power of Python for data science. This course includes hands-on projects and practical applications in data analysis.'
+    },
+    {
+      id: 109,
+      name: "React Native Mobile App Development",
+      author: "Jessica Carter",
+      duration: 24.0,
+      type: "Paid",
+      price: 44.99,
+      ratings: 4.6,
+      image: 'assets/courses/course-img-9.png',
+      description: 'Build cross-platform mobile apps with React Native. This course covers development for iOS and Android platforms.'
+    },
+    {
+      id: 110,
+      name: "Advanced Python Programming",
+      author: "David Anderson",
+      duration: 28.0,
+      type: "Paid",
+      price: 54.99,
+      ratings: 4.8,
+      image: 'assets/courses/course-img-10.webp',
+      description: 'Take your Python skills to the next level with advanced programming techniques, design patterns, and real-world projects.'
+    }
+  ];
+}
