a8f4fdb173f9ccab86c86849f0c2b43b0cd3edda
RohanNAcharya <rohannacharya@gmail.com>
2023-11-28 16:11:53 +0530

    Files

---
+++ 2_Angular_Training/viewChild/src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { DemoComponent } from './demo/demo.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'viewChild';
+
+  @ViewChild('dobInput') dateOfBirth!: ElementRef;
+  @ViewChild('ageInput') age!: ElementRef;
+
+  @ViewChild(DemoComponent, {static: true}) demoComp!: DemoComponent;
+
+  calculateAge(){
+    let birthYear = new Date(this.dateOfBirth.nativeElement.value).getFullYear();
+    let currentYear = new Date().getFullYear();
+    // console.log(this.dateOfBirth);
+    // console.log(this.age);
+    let age = currentYear - birthYear
+    this.age.nativeElement.value = age;
+  }
+
+}
