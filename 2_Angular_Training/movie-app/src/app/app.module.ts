70eefa50147150efcb6be53867b26ecebc8a691a
RohanNAcharya <rohannacharya@gmail.com>
2023-11-30 20:01:04 +0530

    Assignment 2

--- 2_Angular_Training/movie-app/src/app/app.module.ts
+++ 2_Angular_Training/movie-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MovieComponent } from './movie/movie.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { MovieComponent } from './movie/movie.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- 2_Angular_Training/movie-app/src/app/movie/movie.component.ts
+++ 2_Angular_Training/movie-app/src/app/movie/movie.component.ts
@@ -8,24 +8,47 @@ import { Component } from '@angular/core';
 export class MovieComponent {
   movies = [
     {
+      id: 1,
       name: "Adhipurush",
       language: "Hindi",
-      image: "assets/images/movie-1.jpg"
+      image: "assets/images/movie-1.jpg",
+      voted: false
     },
     {
+      id: 2,
       name: "Brahmastra",
       language: "Hindi",
-      image: "assets/images/movie-2.jpg"
+      image: "assets/images/movie-2.jpg",
+      voted: false
     },
     {
+      id: 3,
       name: "The Flash",
       language: "English",
-      image: "assets/images/movie-3.webp"
+      image: "assets/images/movie-3.webp",
+      voted: false
     },
     {
+      id: 4,
       name: "Hoysala",
       language: "Kannada",
-      image: "assets/images/movie-4.webp"
+      image: "assets/images/movie-4.webp",
+      voted: false
     }
   ]
+
+  public onClick(id:number):void{
+    console.log("Clicked!");
+    for(let i of this.movies){
+      if(i.id == id){
+        if(i.voted){
+          i.voted = false;
+        }
+        else{
+          i.voted = true;
+        }
+      }
+    }
+  }
+
 }
