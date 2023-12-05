d68836bc07b3c45d2a58b10ca316837daade23d3
RohanNAcharya <rohannacharya@gmail.com>
2023-12-05 13:46:09 +0530

    Review 2 updates

--- 2_Angular_Training/movie-app/src/app/app.module.ts
+++ 2_Angular_Training/movie-app/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MovieComponent } from './movie/movie.component';
+import { MovieComponent } from './components/movie/movie.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
